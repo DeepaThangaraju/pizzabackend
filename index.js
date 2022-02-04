265fd8cf64c0fc308eaf1b604ff22afa6039d736
DeepaThangaraju <deepat2405@gmail.com>
2022-02-04 13:58:07 +0530

    changes

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ import {userRouter} from "./routes/userRoute.js";
 import {orderRouter} from "./routes/orderRoute.js";
 import { saveorderRouter } from "./routes/saveorderRoute.js";
 import dotenv from 'dotenv';
-import cors from cors;
 
 connect();
 dotenv.config()
 const app=express();
-app.use(cors())
+
 app.use(express.json());
 
 const PORT=process.env.PORT;
